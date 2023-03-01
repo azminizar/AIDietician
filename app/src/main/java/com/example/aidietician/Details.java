72b1c5ef4701d93dae6f47167196a6581a3a9c67
Azmi Nizar <azminizar1781@gmail.com>
2023-03-01 22:40:38 +0530

    Updated details

--- app/src/main/java/com/example/aidietician/Details.java
+++ app/src/main/java/com/example/aidietician/Details.java
@@ -15,23 +15,58 @@ import java.util.ArrayList;
 
 public class Details extends AppCompatActivity {
 
-    Spinner spinner;
+    Spinner spinnerGender,spinnerFoodpref,spinnerDietplan;
     String[] gender = {"Gender","Male","Female","Other"};
+    String[] foodpref = {"Food preference","Vegetarian","Strictly Vegeterian","Non-vegetarian","Vegan"};
+    String[] dietplan = {"Diet plan","Lose weight","Maintain weight","Gain weight"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_details);
 
-        spinner = findViewById(R.id.spinnerGender);
+        spinnerGender = findViewById(R.id.spinnerGender);
+        spinnerFoodpref = findViewById(R.id.spinnerFoodplan);
+        spinnerDietplan = findViewById(R.id.spinnerDietPref);
 
-        ArrayAdapter<String> adapter = new  ArrayAdapter<String>(Details.this, android.R.layout.simple_spinner_item,gender);
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        spinner.setAdapter(adapter);
-        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+        ArrayAdapter<String> adapter1 = new  ArrayAdapter<String>(Details.this, android.R.layout.simple_spinner_item,gender);
+        adapter1.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        spinnerGender.setAdapter(adapter1);
+        spinnerGender.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-                String value = parent.getItemAtPosition(position).toString();
+                String value1 = parent.getItemAtPosition(position).toString();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
+        ArrayAdapter<String> adapter2 = new  ArrayAdapter<String>(Details.this, android.R.layout.simple_spinner_item,foodpref);
+        adapter2.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        spinnerFoodpref.setAdapter(adapter2);
+        spinnerFoodpref.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                String value2 = parent.getItemAtPosition(position).toString();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
+        ArrayAdapter<String> adapter3 = new  ArrayAdapter<String>(Details.this, android.R.layout.simple_spinner_item,dietplan);
+        adapter3.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        spinnerDietplan.setAdapter(adapter3);
+        spinnerDietplan.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                String value3 = parent.getItemAtPosition(position).toString();
             }
 
             @Override
