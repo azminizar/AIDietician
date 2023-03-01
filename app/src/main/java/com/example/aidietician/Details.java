c0e388c5c27f5d6af7d67fc59b4c2870bd74a42a
Azmi Nizar <azminizar1781@gmail.com>
2023-03-01 21:24:45 +0530

    Added weight

--- app/src/main/java/com/example/aidietician/Details.java
+++ app/src/main/java/com/example/aidietician/Details.java
@@ -6,30 +6,34 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
 import android.widget.Spinner;
 
+import com.google.android.material.textfield.TextInputLayout;
+
+import java.util.ArrayList;
+
 public class Details extends AppCompatActivity {
-    Spinner spinner;
-    String[] gender = {"Male","Female","Other"};
+
+    AutoCompleteTextView autotxtView;
+    ArrayList<String> list_gender;
+    ArrayAdapter<String> adap_gender;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_details);
-        spinner = findViewById(R.id.spinnerGender);
 
-        ArrayAdapter<String> adapter = new  ArrayAdapter<String>(Details.this, android.R.layout.simple_spinner_item,gender);
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        spinner.setAdapter(adapter);
-        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-            @Override
-            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-                String value = parent.getItemAtPosition(position).toString();
-            }
+        autotxtView = (AutoCompleteTextView)findViewById(R.id.autotxtView);
 
-            @Override
-            public void onNothingSelected(AdapterView<?> parent) {
+        list_gender = new ArrayList<>();
+        list_gender.add("Male");
+        list_gender.add("Female");
+        list_gender.add("Other");
 
-            }
-        });
+        adap_gender = new ArrayAdapter<>(getApplicationContext(),com.google.android.material.R.layout.support_simple_spinner_dropdown_item,list_gender);
+        autotxtView.setAdapter(adap_gender);
+
+        autotxtView.setThreshold(1);
     }
 }
