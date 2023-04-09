60cad62b7f0fbf77922acfe59bddc380ad98d208
Azmi Nizar <azminizar1781@gmail.com>
2023-04-09 23:39:36 +0530

    Profile fragment updated

--- app/src/main/java/com/example/aidietician/DietFragment.java
+++ app/src/main/java/com/example/aidietician/DietFragment.java
@@ -2,11 +2,17 @@ package com.example.aidietician;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.PopupMenu;
+import android.widget.TextView;
 
 /**
  * A simple {@link Fragment} subclass.
